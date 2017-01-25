9c601fc93f1fef492d26a21fbac8e25f9845000d
Dan Fasciano <daniel.fasciano@uconn.edu>
2017-01-24 21:00:06 -0500

    First SQL declarations and beginnings of user creation system.

---
+++ WebstormProjects/PHP/create_user.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Create User</title>
+    </head>
+    <body>
+        <form>
+            <p>Username: <input type=""text", name="un" /></p>
+            <p>Password: <input type="password", name="pw1" /></p>
+            <p>Confirm Password: <input type="password", name="pw2" /></p>
+            <p><input type="submit" value="Create User" name="s" /></p>
+        </form>
+    </body>
+
+
+
+</html>
