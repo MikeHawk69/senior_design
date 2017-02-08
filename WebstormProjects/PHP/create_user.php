47fb4f9affae6ccbf4bfa7cfcb51179572c5d2f8
Dan Fasciano <daniel.fasciano@uconn.edu>
2017-02-08 17:56:32 -0500

    Worked more on the create_user table.  It is still not functioning correctly though, and is not running the SQL string to insert a new user into the table

--- WebstormProjects/PHP/create_user.php
+++ WebstormProjects/PHP/create_user.php
@@ -4,11 +4,11 @@
     </head>
     <body>
         Create New User
-        <form action="<?php echo htmlspecialchars ($_SERVER["PHP_SELF"]); ?>" method=""post">
-            <p>Username: <input type=""text", name="un" /></p>
-            <p>Password: <input type="password", name="pw1" /></p>
-            <p>Confirm Password: <input type="password", name="pw2" /></p>
-            <p><input type="submit" value="Create User" name="s" /></p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <p><label for="un">Username:</label> <input type="text" name="un" id="un"></p>
+            <p><label for="pw1">Password:</label> <input type="password" name="pw1" id="pw1"></p>
+            <p><label for="pw2">Confirm Password:</label> <input type="password" name="pw2" id="pw2"></p>
+            <p><input type="submit" value="Create User" name="s"></p>
         </form>
         <?php
             if(isset($_POST["s"])) {
@@ -16,26 +16,26 @@
                     echo "Passwords Don't Match.";
                 } else {
                     echo "Connecting to Database...";
-                    $servername = "localhost";
+                    $server_name = "localhost";
                     $username = "root";
                     $password = "root";
                     $dbname = "SURVEY";
-                    $conn = mysqli_connect($servername,$username,$password,$dbname);
+                    $conn = mysqli_connect($server_name,$username,$password,$dbname);
                     if(!$conn) {
-                        die("Could not connect: " . mysql_connect_error());
+                        die("Could not connect: " . mysqli_connect_error());
                     }
-                    echo "Connected to Databse.";
+                    echo "Connected to Database.";
 
                     $un = $_POST["un"];
-                    $salt = "$2a$15$t1h2i3s4i5s6a7s8a9l0t1";
+                    $salt = sprintf("$2a$15$21h2i3s4i5s6a7s8a9l0t1"); //Using CRYPT_BLOWFISH
                     $ps = crypt($_POST["pw1"], $salt);
-                    $ad = 0;
-                    $sql = "INSERT INTO USERTABLE (username, pass admin) VALUES ('$un', '$ps', '$ad')";
+                    //$sql = "INSERT INTO usertable (username, pass admin) VALUES ('$un', '$ps', $ad)";
+                    $sql = "INSERT INTO usertable (username, pass, cid, admin) VALUES ('test', 'password', 1, 0)";
                     echo "<script type='text/javascript'>alert('$sql');</script>";
 
                     if(mysqli_query($conn, $sql)) {
                         echo "New user successfully created.";
-                        echo '<a href="login.php">Login</a>';
+                        //echo "<a href="login.php">Login</a>";
                     } else {
                         echo "Error creating user: " . $conn->error;
                     }
