eba708bd0dcc35777c3dbe4ec7538a39f0697dfc
Dan Fasciano <daniel.fasciano@uconn.edu>
2017-02-10 00:04:48 -0500

    More work on getting user creation and logging in working

--- WebstormProjects/PHP/create_user.php
+++ WebstormProjects/PHP/create_user.php
@@ -4,7 +4,7 @@
     </head>
     <body>
         Create New User
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <form action="<?php ($_SERVER["PHP_SELF"]); ?>" method="post">
             <p><label for="un">Username:</label> <input type="text" name="un" id="un"></p>
             <p><label for="pw1">Password:</label> <input type="password" name="pw1" id="pw1"></p>
             <p><label for="pw2">Confirm Password:</label> <input type="password" name="pw2" id="pw2"></p>
@@ -29,7 +29,8 @@
                     $un = $_POST["un"];
                     $salt = sprintf("$2a$15$21h2i3s4i5s6a7s8a9l0t1"); //Using CRYPT_BLOWFISH
                     $ps = crypt($_POST["pw1"], $salt);
-                    //$sql = "INSERT INTO usertable (username, pass admin) VALUES ('$un', '$ps', $ad)";
+                    $cid = 1;
+                    //$sql = "INSERT INTO usertable (username, pass admin) VALUES ('$un', '$ps', '$cid',0)";
                     $sql = "INSERT INTO usertable (username, pass, cid, admin) VALUES ('test', 'password', 1, 0)";
                     echo "<script type='text/javascript'>alert('$sql');</script>";
 
--- WebstormProjects/PHP/login.php
+++ WebstormProjects/PHP/login.php
@@ -1,7 +1,16 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Dan
- * Date: 2/8/2017
- * Time: 11:08 AM
- */
+<html>
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+        <form name="login" method="post" action="login.php">
+            <p><label for="un">Username:</label>
+                <input type="text" name="un" id="un"></p>
+            <p><label for="pw1">Password:</label>
+                <input type="password" name="pw1" id="pw1"></p>
+            <p><label for=""rm">Remember Me:</label></p>
+                <input type="checkbox" name="rm" value = "1">
+            <p><input type="submit" value="Create User" name="s"></p>
+        </form>
+    </body>
+</html>
